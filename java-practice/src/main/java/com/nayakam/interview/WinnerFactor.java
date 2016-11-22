ccf2a15f7ac7eec0f02cf68b43efa26ef965c6c5
Thillakan Saba <tsaba@itree.com.au>
2016-11-22 20:56:15 +1100

    WinnerFactor

---
+++ java-practice/src/main/java/com/nayakam/interview/WinnerFactor.java
@@ -0,0 +1,44 @@
+package com.nayakam.interview;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * <p>Title: </p>
+ *
+ * <p>Description: Created by tsaba on 22/11/2016.</p>
+ *
+ * @version 1.0
+ */
+public class WinnerFactor {
+
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.print("Enter number of Test Cases: ");
+            int iteration = Integer.parseInt(bufferedReader.readLine());
+
+            while (iteration-- > 0) {
+                System.out.print("Target number: ");
+                int n = Integer.parseInt(bufferedReader.readLine());
+                int sum = 0;
+                if (n == 1) {
+                    sum = 1;
+                } else if (n > 1) {
+                    sum += (1 + n);
+                    for (int i = 2; i <= n / 2; i++) {
+                        if (n % i == 0) {
+                            sum += i;
+                        }
+                    }
+                }
+                System.out.println("Case: " + iteration + ", Number: " + n + ", Target: " + sum);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
